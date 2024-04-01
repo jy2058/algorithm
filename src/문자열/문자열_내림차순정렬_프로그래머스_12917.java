756b89da5ce6eb0fa2ce44ed713f1ddff6edc1a4
jy2058 <jiy3285@gmail.com>
2024-04-01 23:40:06 +0900

    프로그래머스 문자열

---
+++ src/문자열/문자열_내림차순정렬_프로그래머스_12917.java
@@ -0,0 +1,32 @@
+package 문자열;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12917
+ * 12917 / 문자열 내림차순으로 배치하기
+ * 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수
+ * 대문자는 소문자보다 작은 것으로 간주
+ */
+public class 문자열_내림차순정렬_프로그래머스_12917 {
+    public static void main(String[] args) throws IOException {
+        Solution so = new Solution();
+        System.out.println(so.solution("Zbcdefg"));
+    }
+}
+
+class Solution {
+    public String solution(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+
+        StringBuilder sb = new StringBuilder(new String(chars));
+        return sb.reverse().toString();
+    }
+}
+
+/*
+    1. 문자열을 순서대로 정렬
+    2. 역순으로 출력
+ */
---
+++ src/문자열/문자열_자연수뒤집어배열로_프로그래머스_12932.java
@@ -0,0 +1,40 @@
+package 문자열;
+
+import java.io.IOException;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12932
+ * 12932 / 자연수 뒤집어 배열로 만들기
+ *
+ * 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+ */
+public class 문자열_자연수뒤집어배열로_프로그래머스_12932 {
+    public static void main(String[] args) throws IOException {
+        Solution so = new Solution();
+        System.out.println(so.solution(12345));
+    }
+}
+
+class Solution {
+    public int[] solution(long n) {
+        String str = Long.toString(n);
+        // 입력받은 수를 뒤집기
+        StringBuilder sb = new StringBuilder(str).reverse();
+
+        int[] answer = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            // 한글자씩 잘라서 배열에 넣기
+            answer[i] = Integer.parseInt(sb.substring(i, i + 1));
+        }
+        // 배열 출력
+        return answer;
+    }
+}
+
+/*
+    입력 받은 수를 뒤집기 위해 reverse 메서드를 사용했다.
+
+    1. 입력 받은 수 뒤집기
+    2. 한 글자씩 잘라서 배열에 넣기
+    3. 배열 출력
+ */
