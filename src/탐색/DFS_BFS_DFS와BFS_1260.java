0f4095c637393f5c471c1b4146a6215fe008be63
jy2058 <jiy3285@gmail.com>
2024-08-27 13:59:41 +0900

    DFS와BFS_1260

--- src/탐색/DFS_BFS_DFS와BFS_1260.java
+++ src/탐색/DFS_BFS_DFS와BFS_1260.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * https://www.acmicpc.net/problem/1260
+ * 1260 / DFS와 BFS
+ * 실버2
+ *
+ * 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+ */
 public class DFS_BFS_DFS와BFS_1260 {
     private static boolean[] check;
     private static StringBuilder sb;
